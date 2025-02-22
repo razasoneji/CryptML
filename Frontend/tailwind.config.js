c75e0cf5a3df2fe2e24e05cf785a591e0a2bbb39
Shivansh Patel <shivpatel2694@gmail.com>
2025-02-22 22:37:35 +0530

    Added basic signup page

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+      './pages/**/*.{ts,tsx}',
+      './components/**/*.{ts,tsx}',
+      './app/**/*.{ts,tsx}',
+      './src/**/*.{ts,tsx}',
+    ],
+    theme: {
+      extend: {
+        boxShadow: {
+          'input': '0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)',
+        },
+      },
+    },
+    plugins: [require("tailwindcss-animate")],
+  }
